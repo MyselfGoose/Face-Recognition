02c47c0643d2285857c509cfa6793bc8cbf3f973
Azeem Arshad <abubakar20069@gmail.com>
2024-10-31 22:00:44 +0500

    Added a custom css file and attached it to the upload page

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express"
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
